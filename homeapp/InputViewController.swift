d1825c25ebc05eef420232491d933496e4cb9753
Moe <moepua0123@gmai.com>
2020-07-02 17:59:53 +0900

    gitignoreの作成まで実施

---
+++ homeapp/InputViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InputViewController.swift
+//  homeapp
+//
+//  Created by 西山萌花 on 2020/07/02.
+//  Copyright © 2020 moeka.nishiyama. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
